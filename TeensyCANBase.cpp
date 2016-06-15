5043aaf4778f6281b40fda875ea7655b49843412
Carter Turnbaugh <carturn@nuevaschool.org>
2016-06-15 16:50:03 -0700

    Initial attempt at supporting multiple connections in TeensyCANBase

--- TeensyCANBase.cpp
+++ TeensyCANBase.cpp
@@ -1,29 +1,66 @@
 #include <TeensyCANBase.h>
 
-TeensyCANBase::TeensyCANBase(uint32_t id) {
+TeensyCANBase::TeensyCANBase(uint32_t id, int max_queue) {
 	canID = id;
-	CANbus = FlexCAN(1000000);
+	max_queue_len = max_queue;
+	if(CANbus == NULL){
+		copies = 0;
+		CANbus = new FlexCAN(1000000);
+	}
+	else{
+		copies++;
+	}
 }
+
+TeensyCANBase::~TeensyCANBase(){
+	copies--;
+	if(copies <= 0){
+		delete CANbus;
+	}
+}
+
 void TeensyCANBase::begin() {
-	CANbus.begin();
+	if(copies == 0){
+		CANbus->begin();
+	}
 }
 
 void TeensyCANBase::end() {
-	CANbus.end();
+	if(copies == 0){
+		CANbus->end();
+	}
 }
 int TeensyCANBase::available() {
-	return CANbus.available();
+	return CANbus->available();
 }
 
 int TeensyCANBase::read(byte* &msg) {
 	CAN_message_t rxmsg;
 
-	if (CANbus.read(rxmsg)) {
-		if (rxmsg.id == canID){
-			memcpy(msg, rxmsg.buf, 8);
+	for(int i = 0; i < message_queue.size(); i++){
+		if(message_queue[i].id == canID){
+			memcpy(msg, message_queue[i].buf, 8);
+			message_queue.erase(i);
 			return 0;
 		}
 	}
+
+	while(available()){
+		if (CANbus->read(rxmsg)) {
+			if (rxmsg.id == canID){
+				memcpy(msg, rxmsg.buf, 8);
+				return 0;
+			}
+			else{
+				message_queue.push_back(rxmsg);
+				if(message_queue.size() > max_queue_len){
+					// TODO: make less inefficient
+					message_queue.erase(0);
+				}
+			}
+		}
+	}
+	
 	return 1;
 }
 int TeensyCANBase::write(byte* &msg) {
@@ -33,7 +70,7 @@ int TeensyCANBase::write(byte* &msg) {
 	txmsg.len = 8;
 
 	memcpy(txmsg.buf, msg, 8);
-	CANbus.write(txmsg);
+	CANbus->write(txmsg);
 
 	return 0;
 }
--- TeensyCANBase.h
+++ TeensyCANBase.h
@@ -3,13 +3,18 @@
 
 #include <Arduino.h>
 #include <FlexCAN.h>
+#include <vector>
 
 class TeensyCANBase {
 private:
-	FlexCAN CANbus;
+	static FlexCAN * CANbus;
+	static int copies;
+	// There does not seem to be a message queue within FlexCAN, so we need one
+	static vector<CAN_message_t> message_queue;
+	static int max_queue_len;
 public:
 	uint32_t canID;
-	TeensyCANBase(uint32_t id = 0x222);
+	TeensyCANBase(uint32_t id = 0x222, int max_queue = 256);
 	void begin();
 	void end();
 	int available();
